e506c14799814e1446f17a94f725518b49de12f1
amel <akarsic1@etf.unsa.ba>
2018-10-23 16:30:51 +0200

    KLASA PROGRAM 1. KOMIT

---
+++ src/com/company/Program.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.predavanje03;
+
+public class Program {
+
+    public static void main(int arg){
+        Student student1 = new Student("Amel", "Amelovic",   22);
+        Student student2 = new Student("Mujo", "Mujic", 23);
+        Student student3 = new Student("Kare", "Karic", 24);
+        Predmet predmet1 = new Predmet("TP", 2457,40);
+        predmet1.upisi(student1);
+        predmet1.upisi(student2);
+        predmet1.upisi(student3);
+        predmet1.ispisStudenata();
+        predmet1.Ispisi(student1);
+        predmet1.ispisStudenata();
+
+    }
+
+}
